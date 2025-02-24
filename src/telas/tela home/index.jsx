a7399e694aa0fc4c7ce2fca24c600bc69c75a543
Heloisa755 <netomengao19@gmail.com>
2025-02-24 00:19:16 -0300

    atualização

--- src/telas/tela home/index.jsx
+++ src/telas/tela home/index.jsx
@@ -4,11 +4,22 @@ import './style.css'
 function TelaHome() {
   return (
     <div>
-      <div>
+      <div className='div-text01'>
         <h2 className='texto01'>Inicio</h2>
+      </div>
+      <div className='div-text02'>
         <h2 className='texto02'>Resumo</h2>
+      </div>
+      <div className='div-text03'>
         <h2 className='texto03'>Sobre me</h2>
+      </div>
+      <div className='div-text04'>
         <h2 className='texto04'>Contato</h2>
+      </div>
+      <div>
+        <div>
+          <h3 className='text1'>OLa, eu me chamo </h3>
+        </div>
       </div>
     </div>
   );

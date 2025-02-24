5203d10910d33543865a69318284b0820768db85
Heloisa755 <netomengao19@gmail.com>
2025-02-23 23:33:51 -0300

    primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import TelaHome from './telas/tela home/index.jsx'
+
+
+function App() {
+
+  return (
+    <>
+      <TelaHome />
+    </>
+  )
+}
+
+export default App
---
+++ src/telas/tela home/index.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './style.css'
+
+function TelaHome() {
+  return (
+    <div>
+      <div>
+        <h2 className='texto01'>Inicio</h2>
+        <h2 className='texto02'>Resumo</h2>
+        <h2 className='texto03'>Sobre me</h2>
+        <h2 className='texto04'>Contato</h2>
+      </div>
+    </div>
+  );
+}
+
+export default TelaHome;

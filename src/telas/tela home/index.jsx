d46f78481f51dffeaea56709a7ddc0fe653e7474
Neto1919 <netomengao19@gmail.com>
2025-02-25 23:17:11 -0300

    Alualização 25/02/2025

--- src/telas/tela home/index.jsx
+++ src/telas/tela home/index.jsx
@@ -35,15 +35,36 @@ function TelaHome() {
 
       <div className='div04'></div>
       <div>
-        <p className='text4'>TEXTO TEXTO TEXTO</p>
+        <p className='text4'>Sobre mim</p>
+        <p className='text5'>Olá! Meu nome é Espedito Neto. Concluí o curso técnico em Informática no Campus Esperança do IFPB.
+          Durante minha formação técnica, desenvolvi habilidades em programação, especialmente em JavaScript, além de dominar as
+          tecnologias HTML 5 e CSS 3. Também tenho conhecimento em SQL, o que me permite gerenciar e manipular bancos de dados de
+          forma eficiente. Além disso, tenho experiência com frameworks como Nest.js e Vite, ferramentas essenciais para o
+          desenvolvimento de aplicações web modernas.</p>
+          <p className='text6'>Atualmente, estou cursando o Bacharelado em Estatística na Universidade Estadual da Paraíba (UEPB). Estou ansioso para aplicar meus conhecimentos em projetos práticos e continuar desenvolvendo minhas habilidades em tecnologia.</p>
       </div>
       <div>
         <div className='div05'>
           <div className='div06'>
-            <span>TEXTO TEXTO</span>
+            <text className='textcaixa'>Html5</text>
+          </div>
+          <div className='div07'>
+            <text className='textcaixa'>Css3</text>
+          </div>
+          <div className='div08'>
+            <text className='textcaixa'>JavaScript</text>
+          </div>
+          <div className='div09'>
+            <text className='textcaixa'>SQL</text>
+          </div>
+          <div className='div10'>
+            <text className='textcaixa'>Next.js</text>
+          </div>
+          <div className='div11'>
+            <text className='textcaixa'>Vite</text>
           </div>
         </div>
-        <button className='button06'>TEXTO TEXTO</button>
+        <button className='button06'>Download  CV</button>
       </div>
     </div>
   );
